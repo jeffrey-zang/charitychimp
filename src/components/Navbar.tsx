ce182676bb90c15a838e11b44cd1a7104cd96531
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-11-25 20:11:22 -0500

    make pages
    Co-authored by: lifeng-yin <lifeng-yin@proton.me>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import './Navbar.scss'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    const pages = ['Home', 'Browse', 'Search', 'Post']
+    const pages = ['Browse', 'Search', 'Post']
 
     return (
         <div id='nav'>
-            Logo
+            <Link key={'Home'} to={'/'}>Home</Link>
+            {pages.map((value, index) => {
+                return (
+                    <Link key={value} to={`/${value.toLowerCase()}`}>{value}</Link>
+                )
+            })}
         </div>
     )
 }
