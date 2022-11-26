59c1e01d5e8911a0eeba369e79c4065a7da61d42
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-11-25 22:59:25 -0500

    create pages Co-authored-by: Daniel Zhang <d.zhang200788@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import {AppContextProvider} from "./AppContext"
 import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Home from './pages/Home/Home'
+import Browse from './pages/Browse/Browse'
+import Post from './pages/Post/Post'
+import Search from './pages/Search/Search'
 
 function App() {
   return (
     <AppContextProvider>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/browse" element={<Browse/>}/>
+        <Route path="/post" element={<Post/>}/>
+        <Route path='/search' element={<Search/>}/>
+      </Routes>
+      </BrowserRouter>
     </AppContextProvider>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.scss'
 
 const Navbar = () => {
+
+  const pages = ['Browse', 'Post', 'Search']
+
   return (
-    <div>Navbar</div>
+    <div id='nav'>
+      <Link key='Home' to='/'>Home</Link>
+      {pages.map((value) => {
+        return (
+          <Link key={value} to={`/${value.toLowerCase()}`}>{value}</Link>
+        )
+      })}
+    </div>
   )
 }
 
